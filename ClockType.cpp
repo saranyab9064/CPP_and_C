6d4c1c72c74c12ad204875ef123f842b69972189
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-05-01 11:14:53 -0700

    Update ClockType.cpp

--- ClockType.cpp
+++ ClockType.cpp
@@ -7,43 +7,48 @@
 // ========================================================================
 
 1. Implement the class clock type described in the slides, use the same const modifier for all the methods FOR THE METHODS THAT HAVE THE CONST MODIFIER, the ones that don't DO NOT ADD it
-  #include <iostream> using namespace std; class clockType
+ 
+  #include <iostream> using namespace std; 
+ class clockType
 {
-public:
-void setTime(int hours, int minutes, int seconds);
+  public:
+  void setTime(int hours, int minutes, int seconds);
   void getTime(int& hours,int& minutes, int& seconds) const; void printTime() const;
-void incrementSeconds();
-void incrementMinutes();
-void incrementHours();
-bool equalTime(const clockType& otherClock) const; clockType(int hours, int minutes, int seconds); clockType();
-private:
-       int hr;
-       int sec;
-       int min;
+  void incrementSeconds();
+  void incrementMinutes();
+  void incrementHours();
+  bool equalTime(const clockType& otherClock) const; clockType(int hours, int minutes, int seconds); clockType();
+  private:
+         int hr;
+         int sec;
+         int min;
 };
 //Member function definitions
 void clockType::setTime(int hours, int minutes, int seconds) {
-if (0 <= hours && hours < 24) hr = hours;
-else
-hr = 0;
- else }
+    if (0 <= hours && hours < 24) hr = hours;
+    else
+    hr = 0;
+     else 
+}
 if (0 <= minutes && minutes < 60) min = minutes;
 else
-min = 0;
+    min = 0;
 if (0 <= seconds && seconds < 60)
-sec = seconds; sec = 0;
+    sec = seconds; sec = 0;
+
 void clockType::getTime(int& hours, int& minutes, int& seconds) const {
 hours = hr;
  minutes = min; seconds = sec;
 
  }
+
 void clockType::printTime() const
  {
 if (hr < 10)
        cout << "0";
-cout << hr << ":";
+  cout << hr << ":";
 if (min < 10)
-cout << "0"; cout << min << ":";
+  cout << "0"; cout << min << ":";
 if (sec < 10)
        cout << "0";
 cout << sec;
@@ -86,7 +91,7 @@ min = minutes;
 else
 min = 0;
 if (0 <= seconds && seconds < 60)
-sec = seconds; sec = 0;
+ sec = seconds; sec = 0;
 else
  }
 
